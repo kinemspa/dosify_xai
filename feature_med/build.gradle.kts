abba3f092c276e85d5afe6404aeb5d17c590dcf3
KiNeMspA <antonycraig999@gmail.com>
2025-07-22 22:32:22 +1000

    Update libs.versions.toml to latest stable versions as of July 2025

--- feature_med/build.gradle.kts
+++ feature_med/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 dependencies {
     implementation(project(":core"))
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.hilt.navigation.compose)
     implementation(libs.hilt.android)
     implementation(libs.lifecycle.viewmodel.ktx)
     ksp(libs.hilt.compiler)
