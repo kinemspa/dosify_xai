ff4384252eeed7cec73a54a63f9324c62a35ac08
KiNeMspA <antonycraig999@gmail.com>
2025-07-23 17:54:47 +1000

    Add resolution strategy in top-level build.gradle.kts to force kotlin-stdlib 1.9.0 for KSP compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,19 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    // alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.room) apply false
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.android.library) apply false
+}
 
+subprojects {
+    configurations.all {
+        resolutionStrategy {
+            force("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+            force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.0")
+            force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
+        }
+    }
 }
